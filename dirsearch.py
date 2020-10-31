b715894442555230f38017980fc682e0b3c9b256
Mohamed El Ghazali <48661966+mohamedelghazali@users.noreply.github.com>
2020-10-31 01:57:10 +0100

    Update dirsearch.py

--- dirsearch.py
+++ dirsearch.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#  Author: Mauro Soria
+
 
 import sys
 
@@ -23,7 +7,7 @@ if sys.version_info < (3, 0):
     sys.stdout.write("Sorry, dirsearch requires Python 3.x\n")
     sys.exit(1)
 
-from lib.core import ArgumentParser
+from lib.core import ArgumentParser   
 from lib.controller import *
 from lib.output import *
 
